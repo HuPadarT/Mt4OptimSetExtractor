8407136e83d235f3200054366b19d01f1630ae6a
Tibor Pádár <HuPadarT@users.noreply.github.com>
2025-02-28 15:55:13 +0100

    Hogy ne kelljen utánfutó

--- Mt4OptimSetExtractor.dpr
+++ Mt4OptimSetExtractor.dpr
@@ -1,7 +1,7 @@
 program Mt4OptimSetExtractor;
 
 uses
-  Vcl.Forms,
+  Vcl.Forms, midaslib, // hogy ne kelljen melle a dll file is
   uMt4OptimSetExtractor in 'uMt4OptimSetExtractor.pas' {frmMt4OptimSetExtractor},
   uOptimReportModell in 'uOptimReportModell.pas',
   uViewModel in 'uViewModel.pas';
